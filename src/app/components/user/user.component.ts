d267decef4f80deeb4d5112acc4bb25ed9f873d7
Mintnsrtt <yuhj.2549m@gmail.com>
2020-06-23 14:26:50 +0700

    12. Properties & Methods

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -8,5 +8,26 @@ import { Component } from '@angular/core';
 })
 
 export class UserComponent {
+    //Properties
+    fristName = 'Mint';
+    lastName = 'Phon';
+    age = 25;
 
+    //Metthods
+    constructor() {
+      // console.log('Hello User...');
+      // this.sayHello();
+      console.log(this.age);
+      this.hasBirthday();
+      console.log(this.age);
+    }
+
+    sayHello() {
+      console.log(`Hello ${this.fristName}`);
+      // console.log('Hello ' + '' + this.fristName);
+    }
+
+    hasBirthday() {
+      this.age += 1;
+    }
 }
